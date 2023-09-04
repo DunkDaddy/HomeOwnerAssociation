38a7d514ac671bace1940072786c0c71aae48c98
DunkDaddy <59820523+DunkDaddy@users.noreply.github.com>
2023-09-04 13:19:33 +0200

    l

--- HOA/HOAHomepage/views.py
+++ HOA/HOAHomepage/views.py
@@ -17,7 +17,7 @@ def home(response):
     if response.method == "POST":
         registrer = RegistrerForm(response.POST)
         if registrer.is_valid():
-            beboer = Beboer(navn = registrer.cleaned_data['navn'], adresse = registrer.cleaned_data['adresse'], tlf = registrer.cleaned_data['tlf'], brugernavn = registrer.cleaned_data['brugernavn'], password = make_password(registrer.cleaned_data['password']), mail = registrer.cleaned_data['mail'])
+            beboer = Beboer(navn=registrer.cleaned_data['navn'], adresse=registrer.cleaned_data['adresse'], tlf=registrer.cleaned_data['tlf'], brugernavn=registrer.cleaned_data['brugernavn'], password=make_password(registrer.cleaned_data['password']), mail=registrer.cleaned_data['mail'])
             beboer.save()
             return loginpage
         login = LoginForm(response.POST)
